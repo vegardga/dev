264a4dea5c36605d6301ebe46c1631c3cd135be1
Vegard Gamnes <vegard@gamnes.no>
2025-02-17 10:36:00 +0100

    Add dryrun to bash script

--- bash/README.md
+++ bash/README.md
@@ -1,5 +1,31 @@
 # Setup desktop
 
+Basic bash script for setting up a computer.
+
+## Executable
+
+Make scripts executable:
 ```bash
 chmod +x run.sh
+```
+
+Do this for every run script inside the runs folder.
+
+## Dry run
+
+Dry run:
+```bash
+./run.sh --dry
+```
+
+## Filtering
+
+Filtering (for example only run neovim):
+```bash
+./run.sh neovim
+```
+
+Filtering with dryrun:
+```bash
+./run.sh neovim --dry
 ```
--- bash/run.sh
+++ bash/run.sh
@@ -1,18 +1,44 @@
 #!/usr/bin/env bash
 
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-filter="$1"
+filter=""
+dry="0"
 
-echo "$script_dir -- $filter"
+while [[ $# > 0 ]]; do
+	if [[ $1 == "--dry" ]]; then
+		dry="1"
+	else
+		filter="$1"
+	fi
+	shift
+done
+
+log() {
+	if [[ $dry == "1" ]]; then
+		echo "[DRY_RUN]: $@"
+	else
+		echo "$@"
+	fi
+}
+
+execute() {
+	log "execute $@"
+	if [[ $dry == "1" ]]; then
+		return
+	fi
+	"$@"
+}
+
+log "$script_dir -- $filter"
 
 cd $script_dir
 scripts=$(find ./runs -maxdepth 1 -mindepth 1 -type f)
 
 for script in $scripts; do
 	if echo "$script" | grep -qv "$filter"; then
-		echo "filtering $script"
+		log "filtering $script"
 		continue
 	fi
 
-	./$script
+	execute ./$script
 done
