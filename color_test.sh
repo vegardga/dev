c29e404e8fdab7bd7be7bd71bf1ec51540ce42ee
Vegard Gamnes <vegard@gamnes.no>
2026-03-03 20:21:44 +0100

    Bash script with colors - testing

---
+++ color_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BLACK='\033[30m'
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+MAGENTA='\033[35m'
+CYAN='\033[36m'
+WHITE='\033[37m'
+RESET='\033[0m'
+
+echo -e "${RED}Error: Something went wrong!${RESET}"
+echo -e "${GREEN}Success: Operation completed.${RESET}"
+echo -e "${YELLOW}Warning: Check your input!${RESET}"
